5508e758e6d64775547fef82fdc65548ef6509b0
Signynt <67801159+Signynt@users.noreply.github.com>
2022-12-01 15:00:33 +0100

    updated solution for part 2

--- Day 1 - Calorie Counting/calorie_couting.py
+++ Day 1 - Calorie Counting/calorie_couting.py
@@ -7,11 +7,6 @@ def convert_list_to_int(list):
     int_list = [int(item) for item in list if item]
     return int_list
 
-def find_max_calories(list):
-    max_value = max(list)
-    index = list.index(max_value)
-    return index, max_value
-
 def sum_calories_for_each_elf(list):
     list_of_summed_calories = []
     for item in list:
@@ -21,10 +16,12 @@ def sum_calories_for_each_elf(list):
         list_of_summed_calories.append(summed_calories)
     return list_of_summed_calories
 
-def output_result(list):
-    result_index = find_index_of_max(list)[0]
-    result_calories = find_index_of_max(list)[1]
-    print("The elf carrying the most calories is elf number " + str(result_index) + ", who is carrying " + str(result_calories) + " calories.")
+def output_result_top_x_elves(list, top_x_elves):
+    sorted_list = sorted(list, reverse=True)[:top_x_elves]
+    result_calories = sum(sorted_list)
+    result_index = [list.index(item) for item in sorted_list]
+    print("The top " + str(top_x_elves) + " elves are " + str(result_index) + " and are carrying " + str(result_calories) + " calories total.")
 
 list_split_up_by_elf = load_calories_file('calories.txt').split('\n\n')
-output_result(sum_calories_for_each_elf(list_split_up_by_elf))
+
+output_result_top_x_elves(sum_calories_for_each_elf(list_split_up_by_elf), 3)
